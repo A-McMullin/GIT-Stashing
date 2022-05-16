8c02f74e6b5900af7455d9a50737cc59b83dc250
@A-McMullin <a.p.mcmullin@gmail.com>
2022-05-15 21:56:57 -0500

    the settings of the car are set to 80MHz

--- src/StashingClass/Main.java
+++ src/StashingClass/Main.java
@@ -10,6 +10,10 @@ public class Main {
         Audi.engineCap ="V8";
         Audi.headlights ="blue";
         Audi.regNo ="123ABC";
+
+        System.out.println("welcome to my car");
+        System.out.println("the radio is set to 80MHz");
+        System.out.println("turn on the air conditioner");
         
     }
 }
